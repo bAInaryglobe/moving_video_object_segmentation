be3c339d8bc3c10cc98ce534c74915ae0c3a61f5
Saeed Ahmad <saeedahmad.icp@gmail.com>
2023-09-07 19:48:57 +0900

    Update yolo_segment.py

--- yolo_segment.py
+++ yolo_segment.py
@@ -1,20 +1,3 @@
-# import os
 
-# import torch
-# from ultralytics import YOLO
-
-
-
-# if __name__ == '__main__':
-#     video_path = os.path.join('src', 'classroom.mp4')
-    
-#     ## check for device
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     yolo_model = YOLO('yolov8x-seg.pt', task='segment',).to(device)
-    
-#     predictions = yolo_model(video_path, conf=0.25, classes=[0], save_dir='output')
-    
- 
-    
     
     
